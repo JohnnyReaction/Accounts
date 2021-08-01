5dbe936983eafb903d6c2444577c21196b937096
JohnnyReaction <40791710+JohnnyReaction@users.noreply.github.com>
2021-08-01 20:41:11 +0100

    Working initial commit

---
+++ DB_Accounts/Accounts/dbo/Tables/Expenses.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Expenses] (
+    [TransID]     INT             NOT NULL,
+    [TrDate]      DATE            NOT NULL,
+    [CatID]       INT             NOT NULL,
+    [Category]    VARCHAR (50)    NOT NULL,
+    [SubCatID]    INT             NOT NULL,
+    [SubCategory] VARCHAR (50)    NOT NULL,
+    [Amount]      DECIMAL (18, 2) NOT NULL,
+    CONSTRAINT [PK_Expenses] PRIMARY KEY CLUSTERED ([TransID] ASC)
+);
+
